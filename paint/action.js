ff18b8a7ffba2584a7cc209326fdc5f3a2866266
Noah <noahleemccarver@gmail.com>
2025-10-22 11:41:15 -0500

    big changes

---
+++ paint/action.js
@@ -0,0 +1,10 @@
+const canvasSize = 256;
+
+function setup() {
+    let canvas = createCanvas(canvasSize, canvasSize);
+    canvas.parent('canvasContainer');
+}
+
+function draw() {
+    background(0);
+}
