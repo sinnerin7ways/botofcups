7624af887e8b56fd177516701a855d7e46b11f52
Noah <noahleemccarver@gmail.com>
2025-10-22 20:23:52 -0500

    rand bg

--- paint/action.js
+++ paint/action.js
@@ -1,10 +1,51 @@
 const canvasSize = 256;
 
+let bgColor = [0, 0, 0];
+let bgdColor = [0, 0, 0];
+let fgColor = [0, 0, 0];
+let olColor = [0, 0, 0];
+let bgdColorOption = '';
+let fgColorOption = '';
+let olColorOption = '';
+
+
+function randomColors () {
+    bgColor = [
+        Math.round(Math.random() * 255),
+        Math.round(Math.random() * 255),
+        Math.round(Math.random() * 255)
+    ];
+    fgColor = [
+        Math.round(Math.random() * 255),
+        Math.round(Math.random() * 255),
+        Math.round(Math.random() * 255)
+    ];
+    olColor = [
+        Math.round(Math.random() * 255),
+        Math.round(Math.random() * 255),
+        Math.round(Math.random() * 255)
+    ];
+}
+window.randomColors = randomColors;
+
+function circularBG () {
+
+}
+
 function setup() {
     let canvas = createCanvas(canvasSize, canvasSize);
     canvas.parent('canvasContainer');
+
 }
 
 function draw() {
-    background(0);
+    background(bgColor);
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const colorButton = document.getElementById('randColors');
+    if (colorButton) {
+        colorButton.addEventListener('click', () => {
+        randomColors()})
+    }
+});
