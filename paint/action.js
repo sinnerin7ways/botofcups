67c8a34eca3bfdf5730d1ce569a16e7234ab900d
Noah <noahleemccarver@gmail.com>
2025-10-23 20:57:25 -0500

    update

--- paint/action.js
+++ paint/action.js
@@ -1,36 +1,52 @@
 const canvasSize = 256;
 
-let bgColor = [0, 0, 0];
-let bgdColor = [0, 0, 0];
-let fgColor = [0, 0, 0];
-let olColor = [0, 0, 0];
-let bgdColorOption = '';
-let fgColorOption = '';
-let olColorOption = '';
+//bg color, dg detail color, fg color, ol color
+var choices = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], 'none', 'none', 'none'];
 
 
-function randomColors () {
-    bgColor = [
+function savePainting() {
+    console.log('save');
+}
+
+function randomColors (choices) {
+    choices[0] = [
         Math.round(Math.random() * 255),
         Math.round(Math.random() * 255),
         Math.round(Math.random() * 255)
     ];
-    fgColor = [
+    choices[1] = [
         Math.round(Math.random() * 255),
         Math.round(Math.random() * 255),
         Math.round(Math.random() * 255)
     ];
-    olColor = [
+    choices[2] = [
+        Math.round(Math.random() * 255),
+        Math.round(Math.random() * 255),
+        Math.round(Math.random() * 255)
+    ];
+    choices[3] = [
         Math.round(Math.random() * 255),
         Math.round(Math.random() * 255),
         Math.round(Math.random() * 255)
     ];
 }
+
+function circularBG (choices) {
+    const minCeiled = Math.ceil(4);
+    const maxFloored = Math.floor(10);
+    circlesN = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+
+    stroke(choices[1]);
+
+    for (let i = 0; i < circlesN; i++) {
+        var 
+        
+        drawX = Math
+    }
+}
+
 window.randomColors = randomColors;
-
-function circularBG () {
-
-}
+window.savePainting = savePainting;
 
 function setup() {
     let canvas = createCanvas(canvasSize, canvasSize);
@@ -38,14 +54,39 @@ function setup() {
 
 }
 
+
 function draw() {
-    background(bgColor);
+    background(choices[0]);
 }
+
 
 document.addEventListener('DOMContentLoaded', () => {
     const colorButton = document.getElementById('randColors');
     if (colorButton) {
         colorButton.addEventListener('click', () => {
-        randomColors()})
+        randomColors(choices)})
     }
+    const savePNG = document.getElementById('savePNG');
+    if (savePNG) {
+        savePNG.addEventListener('click', () => {
+        savePainting()})
+    }
+    const bgRadios = document.getElementsByName('bgSelect');
+    bgRadios.forEach(radio => {
+        radio.addEventListener('change', () => {
+            choices[4] = radio.value;
+        })
+    })
+    const fgRadios = document.getElementsByName('fgSelect');
+    fgRadios.forEach(radio => {
+        radio.addEventListener('change', () => {
+            choices[5] = radio.value;
+        })
+    })
+    const olRadios = document.getElementsByName('olSelect');
+    olRadios.forEach(radio => {
+        radio.addEventListener('change', () => {
+            choices[6] = radio.value;
+        })
+    })
 });
